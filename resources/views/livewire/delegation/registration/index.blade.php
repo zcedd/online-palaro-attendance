3b39e468f61790f39171db7130a46cbf4d55574f
Zcedd <zcedbuduan@gmail.com>
2025-01-24 16:22:52 +0800

    style: certification before submitting the list of delegate

--- resources/views/livewire/delegation/registration/index.blade.php
+++ resources/views/livewire/delegation/registration/index.blade.php
@@ -11,4 +11,28 @@
     <div class="mb-3">
         <livewire:delegation.registration.team-member-section />
     </div>
+
+    <x-form-section-vertical submit="submit">
+        <x-slot name="title">
+            Certification and Submission
+        </x-slot>
+        <x-slot name="description">
+            By submitting this form, you agree to the terms and conditions of the Palarong Pambansa 2025.
+        </x-slot>
+        <x-slot name="form">
+            <div class="mb-2">
+                <div class="form-check">
+                    <x-checkbox id="certify" wire:model="certify"
+                        class="{{ $errors->has('certify') ? 'is-invalid' : '' }}" />
+                    <x-label class="form-label" for="certify">I certify that the information provided is true and
+                        correct.
+                    </x-label>
+                    <x-input-error for="certify" />
+                </div>
+            </div>
+        </x-slot>
+        <x-slot name="actions">
+            <x-button type="submit" class="btn-primary">Submit</x-button>
+        </x-slot>
+    </x-form-section-vertical>
 </div>
